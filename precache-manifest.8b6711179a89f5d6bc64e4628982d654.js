6a2d25b020c74229f0e2080ca5941e5b3b14dc03
Cristina.Alekseeva <cristina.alekseeva@gmail.com>
2020-06-29 00:30:15 +0300

    Updates

---
+++ precache-manifest.8b6711179a89f5d6bc64e4628982d654.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7db1f1838dc2c7a3cd48b045eedbc7",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "37aac51349f5e015c007",
+    "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7522c8c9fb072f2dddb4",
+    "url": "/react-portfolio/static/css/main.f6a1aff9.chunk.css"
+  },
+  {
+    "revision": "37aac51349f5e015c007",
+    "url": "/react-portfolio/static/js/2.4b9289ae.chunk.js"
+  },
+  {
+    "revision": "7522c8c9fb072f2dddb4",
+    "url": "/react-portfolio/static/js/main.0986ab3c.chunk.js"
+  },
+  {
+    "revision": "5c15836319dc440961f8",
+    "url": "/react-portfolio/static/js/runtime~main.92964280.js"
+  },
+  {
+    "revision": "4d765fdda7420f5a61eb36e0990dba53",
+    "url": "/react-portfolio/static/media/Satellite.4d765fdd.otf"
+  },
+  {
+    "revision": "1ae4f8f500201244ac26323c5bfbbec6",
+    "url": "/react-portfolio/static/media/catclose.1ae4f8f5.png"
+  },
+  {
+    "revision": "3249b9e251bf5768141cd4b0f389ebfa",
+    "url": "/react-portfolio/static/media/catsketch.3249b9e2.png"
+  }
+]);

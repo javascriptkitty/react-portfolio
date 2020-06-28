b07c77593694113ba2d999b810ff1873576212dc
Cristina.Alekseeva <cristina.alekseeva@gmail.com>
2020-06-29 00:12:04 +0300

    add to gh-pages

---
+++ build/precache-manifest.b39a731637d13f614ed563bc3355ceba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c877a300ab4596b6a069bb6c073e91a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d96644863b838c8c0734",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0a22ef427d4d1e4f9cb2",
+    "url": "/static/css/main.f81f1933.chunk.css"
+  },
+  {
+    "revision": "d96644863b838c8c0734",
+    "url": "/static/js/2.43871241.chunk.js"
+  },
+  {
+    "revision": "0a22ef427d4d1e4f9cb2",
+    "url": "/static/js/main.78813e34.chunk.js"
+  },
+  {
+    "revision": "ffe7a78fb29c46dfc526",
+    "url": "/static/js/runtime~main.3d9d3c2e.js"
+  },
+  {
+    "revision": "4d765fdda7420f5a61eb36e0990dba53",
+    "url": "/static/media/Satellite.4d765fdd.otf"
+  },
+  {
+    "revision": "1ae4f8f500201244ac26323c5bfbbec6",
+    "url": "/static/media/catclose.1ae4f8f5.png"
+  },
+  {
+    "revision": "3249b9e251bf5768141cd4b0f389ebfa",
+    "url": "/static/media/catsketch.3249b9e2.png"
+  }
+]);
